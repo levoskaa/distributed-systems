4e831d419beff14530949e21ffd8e84ce5a2d0dd
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-09-17 17:38:24 +0200

    feat(webapi): add DB context

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using WebAPI.Data;
 
-namespace ElosztottLabor
+namespace WebAPI
 {
     public class Startup
     {
@@ -25,6 +21,8 @@ namespace ElosztottLabor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<HEMDbContext>(opt => opt.UseInMemoryDatabase("HEMDb"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
