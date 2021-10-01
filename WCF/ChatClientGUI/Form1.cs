6de30e4546d8de401ba6f6cd396d5fae9537e549
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-01 19:39:01 +0200

    feat(wcf): implement tasks 5.1-5.2

---
+++ WCF/ChatClientGUI/Form1.cs
@@ -0,0 +1,28 @@
+﻿using ChatClientGUI.ServiceReference1;
+using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace ChatClientGUI
+{
+    public partial class Form1 : Form, IChatServiceCallback
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Receive(ChatMessage m)
+        {
+            MessageBox.Show(m.Text);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var proxy = new ChatServiceClient(new InstanceContext(this));
+            var name = Guid.NewGuid().ToString();
+            proxy.Register(name);
+            proxy.SendAsync(new ChatMessage { Target = name, Text = "Hello, Leo" });
+        }
+    }
+}
